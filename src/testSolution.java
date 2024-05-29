76511916b024f1e3a026f93974237d2c3786de72
sayhitosumit <sumitranjan.rocks@gmail.com>
2024-05-30 00:44:46 +0530

    corrected prev seq problem

--- src/testSolution.java
+++ src/testSolution.java
@@ -2,56 +2,23 @@ public class testSolution {
 
     public int longestOnes(int[] nums, int k) {
 
-        int longest=0;
-        for (int i = 0; i < nums.length ; i++) {
-            if(nums[i]==1){
-               int count =0;
-               int kd=k;
-                for (int j = i; j < nums.length; j++) {
-                    if(nums[j]==1) {
-                        count++;
-                    }
-                    else {
-                        if(kd>0) {
-                            count++;
-                            kd--;
-                        }
-                        else {
-                            longest=Math.max(longest,count);
-                            break;
-                        }
+        int max = 0;
+        int l = 0;
+        int r = 0;
+// nums = [0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1], k = 3
+        while (r < nums.length) {
 
-                    }
+            if (nums[r] == 0) k--;
 
-                }
-            } else {
-                int count =0;
-                int kd=k-1;
-                int flag=0;
-                for (int j = i; j < nums.length; j++) {
-                    if(nums[j]==1) {
-                        count++;
-                        if(flag==0) count++;
-                        flag=1;
-                    }
-                    else {
-                        if(kd>0) {
-                            count++;
-                            kd--;
-                        }
-                        else {
-                            longest=Math.max(longest,count);
-                            break;
-                        }
-
-                    }
-
-                }
-
+            while (k < 0) {
+                if (nums[l] == 0) k++;
+                l++;
             }
-        }
+            max = Math.max(max, r - l + 1);
+          r++;
 
-       return longest;
+        }
+        return max;
     }
 
 
