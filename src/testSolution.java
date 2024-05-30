0a96536afdcd47d548cd75d47362ee5a4affd76b
sayhitosumit <sumitranjan.rocks@gmail.com>
2024-05-31 00:27:16 +0530

    Find the Difference of Two Arrays

--- src/testSolution.java
+++ src/testSolution.java
@@ -1,3 +1,8 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class testSolution {
 
     public int longestOnes(int[] nums, int k) {
@@ -20,7 +25,54 @@ public class testSolution {
         }
         return max;
     }
+    public int largestAltitude(int[] gain) {
+        int altitude =0;
+        int highestAltitude =0;
 
+        for (int i = 0; i <gain.length ; i++) {
+        altitude+=gain[i];
+        if(altitude>highestAltitude) highestAltitude = altitude;
+
+
+        }
+
+
+        return highestAltitude;
+    }
+
+
+    public List<List<Integer>> findDifference(int[] nums1, int[] nums2) {
+        Set<Integer> diff1 = new HashSet<>();
+        Set<Integer> diff2 = new HashSet<>();
+        List<Integer> diff3 = new ArrayList<>();
+        List<Integer> diff4 = new ArrayList<>();
+        List<List<Integer>> list = new ArrayList<>();
+
+        for (int i :
+                nums1) {
+            diff1.add(i);
+        }
+
+        for (int i :
+                nums2) {
+            diff2.add(i);
+        }
+
+        for (int i :
+                diff1) {
+            if(!diff2.contains(i)) diff3.add(i);
+        }
+        for (int i :
+                diff2) {
+            if(!diff1.contains(i)) diff4.add(i);
+        }
+
+        list.add(diff3);
+        list.add(diff4);
+
+        return list;
+
+    }
 
     public static void main(String[] args) {
 
